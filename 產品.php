ea44d3bb6abdd8bb496cb627d9b9fb1d76f55e2f
time-002 <40518114@gcloud.csu.edu.tw>
2020-06-03 14:35:06 +0800

    取得資料庫產品資料

--- 產品.php
+++ 產品.php
@@ -1,4 +1,8 @@
 <?php include "addmysql.php";?>
+<?php
+    $query_cakes = "SELECT * FROM `cakes` WHERE 1";
+    $stmt = $db_link->query($query_cakes);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -43,63 +47,17 @@
     <div class="container my-5">
         <div class="row justify-content-center">
             <div class="card-columns col-md-12 col-10 p-0">
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/柚香黑烏龍乳酪蛋糕.jpg" >
-                    <div class="card-body">
-                        <h4 class="card-title">柚香黑烏龍乳酪蛋糕</h4>
-                        <p class="card-text">$200<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/cake-3.jpg" >
-                    <div class="card-body">
-                        <h4 class="card-title">某蛋糕</h4>
-                        <p class="card-text">$250<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/草莓黑森林蛋糕.jpg" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">草莓黑森林蛋糕</h4>
-                        <p class="card-text">$350<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/生酮蛋糕.jpg" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">生酮蛋糕</h4>
-                        <p class="card-text">$80<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/玫瑰伯爵蛋糕.jpg" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">玫瑰伯爵蛋糕</h4>
-                        <p class="card-text">$180<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/大甲芋泥蛋糕.jpg" >
-                    <div class="card-body">
-                        <h4 class="card-title">大甲芋泥蛋糕</h4>
-                        <p class="card-text">$666<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/cake-2.jpg" >
-                    <div class="card-body">
-                        <h4 class="card-title">冰淇淋莓果蛋糕</h4>
-                        <p class="card-text">$399<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-                <div class="card">
-                    <img class="card-img-top" src="img/原圖/瑰香頌乳酪慕斯.jpg" >
-                    <div class="card-body">
-                        <h4 class="card-title">瑰香頌乳酪慕斯</h4>
-                        <p class="card-text">$50<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
-                    </div>
-                </div>
-
+                <?php 
+                    while($row = $stmt->fetch_assoc()){
+                        echo '<div class="card">
+                        <img class="card-img-top" src="img/原圖/'.$row["Name"].'.jpg">
+                        <div class="card-body">
+                            <h4 class="card-title">'.$row["Name"].'</h4>
+                            <p class="card-text">$'.$row["price"].'<button type="button" class="btn btn-primary addcart">加入購物車</button></p>
+                        </div>
+                    </div>';
+                    };
+                ?>
             </div>
         </div>
     </div>
