bccaf2be1ae49f35d2ce3d9887e79a43143b99d6
time-002 <40518114@gcloud.csu.edu.tw>
2020-06-15 18:33:03 +0800

    新增訂購資料表

--- sql/dessertshop.sql
+++ sql/dessertshop.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- 主機: 127.0.0.1:3307
--- 產生時間： 2020-06-03 08:39:55
--- 伺服器版本: 10.1.37-MariaDB
--- PHP 版本： 7.3.1
+-- 主機： 127.0.0.1
+-- 產生時間： 2020-06-15 12:31:09
+-- 伺服器版本： 10.4.11-MariaDB
+-- PHP 版本： 7.4.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -36,7 +35,7 @@ CREATE TABLE `cakes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- 資料表的匯出資料 `cakes`
+-- 傾印資料表的資料 `cakes`
 --
 
 INSERT INTO `cakes` (`Name`, `price`, `Quantity`, `Introduction`) VALUES
@@ -66,13 +65,13 @@ CREATE TABLE `memberdata` (
   `m_url` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `m_phone` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `m_address` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `m_login` int(11) UNSIGNED NOT NULL DEFAULT '0',
+  `m_login` int(11) UNSIGNED NOT NULL DEFAULT 0,
   `m_logintime` datetime DEFAULT NULL,
   `m_jointime` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- 資料表的匯出資料 `memberdata`
+-- 傾印資料表的資料 `memberdata`
 --
 
 INSERT INTO `memberdata` (`m_id`, `m_name`, `m_username`, `m_passwd`, `m_sex`, `m_birthday`, `m_level`, `m_email`, `m_url`, `m_phone`, `m_address`, `m_login`, `m_logintime`, `m_jointime`) VALUES
@@ -81,8 +80,57 @@ INSERT INTO `memberdata` (`m_id`, `m_name`, `m_username`, `m_passwd`, `m_sex`, `
 (15, 'Kevin', 'testacct001', '$2y$10$aiL/xpQEx2qgO3aaE/vSFOqUT5CAAME.SOQrj8H5l0ANCHcaVYseO', '男', '2016-06-16', 'member', 'gihnd4v3i1n@gmail.com', '', '0977756123', '10500 Quivira Rd, Overland Park, KS 66200, United States', 1, '2020-05-22 18:44:40', '2020-05-22 18:44:14'),
 (16, '欣瑩', 'testacct002', '$2y$10$rFDfEt21MRD.pKZZMBFYWe87qpFtPl6GGrNn/a6JswP4LNPi9Sl0i', '女', '2011-11-16', 'member', 'shina043v@gmail.com', '', '0912345444', 'Tiwan', 1, '2020-05-22 18:51:18', '2020-05-22 18:51:10');
 
+-- --------------------------------------------------------
+
 --
--- 已匯出資料表的索引
+-- 資料表結構 `orderlist_cake`
+--
+
+CREATE TABLE `orderlist_cake` (
+  `id` int(11) NOT NULL,
+  `acct` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `order_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `orderlist_cake`
+--
+
+INSERT INTO `orderlist_cake` (`id`, `acct`, `order_id`, `name`, `quantity`, `price`, `order_date`) VALUES
+(1, 'testacct000', 1, '生酮蛋糕', 1, 329, '2020-06-15 18:29:26');
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `orderlist_member`
+--
+
+CREATE TABLE `orderlist_member` (
+  `id` int(11) NOT NULL,
+  `acct` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `member_name` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `order_price` int(11) NOT NULL,
+  `order_address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `order_date` datetime NOT NULL,
+  `pickup_date` datetime NOT NULL,
+  `order_staus` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `orderlist_member`
+--
+
+INSERT INTO `orderlist_member` (`id`, `acct`, `member_name`, `phone`, `order_id`, `order_price`, `order_address`, `order_date`, `pickup_date`, `order_staus`) VALUES
+(1, 'testacct000', '瑪雅', '0000', 1, 329, '???', '2020-06-15 18:26:25', '2020-06-24 18:26:25', '未領取');
+
+--
+-- 已傾印資料表的索引
 --
 
 --
@@ -99,14 +147,39 @@ ALTER TABLE `memberdata`
   ADD UNIQUE KEY `m_username` (`m_username`);
 
 --
--- 在匯出的資料表使用 AUTO_INCREMENT
+-- 資料表索引 `orderlist_cake`
+--
+ALTER TABLE `orderlist_cake`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `orderlist_member`
+--
+ALTER TABLE `orderlist_member`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
 --
--- 使用資料表 AUTO_INCREMENT `memberdata`
+-- 使用資料表自動遞增(AUTO_INCREMENT) `memberdata`
 --
 ALTER TABLE `memberdata`
   MODIFY `m_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `orderlist_cake`
+--
+ALTER TABLE `orderlist_cake`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `orderlist_member`
+--
+ALTER TABLE `orderlist_member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
