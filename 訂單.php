86f7b7655b9e8019832799f2eb46e9eda0c78495
time-002 <40518114@gcloud.csu.edu.tw>
2020-07-01 04:10:49 +0800

    新增管理者訂單查看

--- 訂單.php
+++ 訂單.php
@@ -30,47 +30,61 @@
         <div class="row">
             <!-- 以下內容以迴圈建立 -->
             <?php
-                #取得會員資料
-                $query_RecMember = "SELECT * FROM memberdata WHERE m_username = '{$_SESSION["loginMember"]}'";
-                $RecMember = $db_link->query($query_RecMember);
-                $row_RecMember = $RecMember->fetch_assoc();
-                #取得會員訂單資料
-                $query_order = "SELECT * FROM `orderlist_member` WHERE acct='{$row_RecMember["m_username"]}'";
-                $stmt_order = $db_link->query($query_order);
-                #建立
-                while($row = $stmt_order->fetch_assoc()){
-                    #取得每份訂單的蛋糕資料
-                    $query_order_cake = "SELECT * FROM `orderlist_cake` WHERE order_id='{$row["order_id"]}'";
-                    $stmt_order_cake = $db_link->query($query_order_cake);
-
-                    echo '<div class="col-lg-4 col-md-6">
-                            <div class="olist" style="border: 1px solid #f2e5b1;background-color: #243560;padding: 10px;">
-                                <div class="mb-1" style="font-size: 1.5rem;">取貨日: '.$row['pickup_date'].'</div>
-                                <div class="w-100 mb-1">編號: '.$row['order_id'].'</div>
-                                <div class="w-50 mb-1">帳號: '.$row['acct'].'</div>
-                                <div class="w-50 mb-1">取貨人: '.$row['member_name'].'</div>
-                                <div class="w-50 mb-1">手機: '.$row['phone'].'</div>
-                                <div class="w-50 mb-1">取貨店: '.$row['order_address'].'</div>
-                                <div class="w-50 mb-1">狀態: '.$row['order_staus'].'</div>
-                                <div class="w-50 mb-1">備註: '.$row['remarks'].'</div>
-                                <a class="w-100" data-toggle="collapse" href="#contentId" aria-expanded="false" aria-controls="contentId">
-                                    選購項目
-                                </a>
-                                <div class="collapse w-100 mb-1" id="contentId">';
-                    while($row_cake = $stmt_order_cake->fetch_assoc()){
-                    echo            '<div class="d-flex flex-wrap">
-                                        <div class="col-6">'.$row_cake['name'].'</div>
-                                        <div class="col-3">x'.$row_cake['quantity'].'</div>
-                                        <div class="col-3">$'.$row_cake['price_total'].'</div>
-                                    </div>';
+                if(isset($_SESSION["memberLevel"])){
+                    if($_SESSION["memberLevel"] == "member"){
+                        #取得會員資料
+                        $query_RecMember = "SELECT * FROM memberdata WHERE m_username = '{$_SESSION["loginMember"]}'";
+                        $RecMember = $db_link->query($query_RecMember);
+                        $row_RecMember = $RecMember->fetch_assoc();
+                        #取得會員訂單資料
+                        $query_order = "SELECT * FROM `orderlist_member` WHERE acct='{$row_RecMember["m_username"]}'";
+                        $stmt_order = $db_link->query($query_order);
                     }
-                    echo        '</div>
-                                <div class="w-100 d-flex flex-wrap justify-content-between">
-                                    <div>訂購日: '.$row['order_date'].'</div>
-                                    <div>合計: $'.$row['order_price'].'</div>
+                    else if($_SESSION["memberLevel"] == "admin"){
+                        #取得ALL會員訂單資料
+                        $query_order = "SELECT * FROM `orderlist_member` WHERE 1 ORDER BY pickup_date ASC";
+                        $stmt_order = $db_link->query($query_order);
+                    }
+                    
+                        
+                    #建立
+                    while($row = $stmt_order->fetch_assoc()){
+                        #取得每份訂單的蛋糕資料
+                        $query_order_cake = "SELECT * FROM `orderlist_cake` WHERE order_id='{$row["order_id"]}'";
+                        $stmt_order_cake = $db_link->query($query_order_cake);
+
+                        echo '<div class="col-lg-4 col-md-6">
+                                <div class="olist" style="border: 1px solid #f2e5b1;background-color: #243560;padding: 10px;">
+                                    <div class="mb-1" style="font-size: 1.5rem;">取貨日: '.$row['pickup_date'].'</div>
+                                    <div class="w-100 mb-1">編號: '.$row['order_id'].'</div>
+                                    <div class="w-50 mb-1">帳號: '.$row['acct'].'</div>
+                                    <div class="w-50 mb-1">取貨人: '.$row['member_name'].'</div>
+                                    <div class="w-50 mb-1">手機: '.$row['phone'].'</div>
+                                    <div class="w-50 mb-1">取貨店: '.$row['order_address'].'</div>
+                                    <div class="w-50 mb-1">狀態: '.$row['order_staus'].'</div>
+                                    <div class="w-50 mb-1">備註: '.$row['remarks'].'</div>
+                                    <a class="w-100" data-toggle="collapse" href="#contentId" aria-expanded="false" aria-controls="contentId">
+                                        選購項目
+                                    </a>
+                                    <div class="collapse w-100 mb-1" id="contentId">';
+                        while($row_cake = $stmt_order_cake->fetch_assoc()){
+                        echo            '<div class="d-flex flex-wrap">
+                                            <div class="col-6">'.$row_cake['name'].'</div>
+                                            <div class="col-3">x'.$row_cake['quantity'].'</div>
+                                            <div class="col-3">$'.$row_cake['price_total'].'</div>
+                                        </div>';
+                        }
+                        echo        '</div>
+                                    <div class="w-100 d-flex flex-wrap justify-content-between">
+                                        <div>訂購日: '.$row['order_date'].'</div>
+                                        <div>合計: $'.$row['order_price'].'</div>
+                                    </div>
                                 </div>
-                            </div>
-                          </div>';
+                            </div>';
+                    }
+                }
+                else{
+
                 }
             ?>
         </div>
